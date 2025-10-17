1b573421874026d4c71fedd60b2e901d60f75333
cvattath <christyvk.chris@gmail.com>
2025-10-17 14:03:25 -0700

    Add PHP lab files and README for expressions and control flow

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # cosc213-php-lab-expressions-control-flow
+This is the readme file for Lab6
---
+++ lab4/01_expressions.php
@@ -0,0 +1,20 @@
+<?php
+$a = 10; $b = 3;
+$sum = $a + $b;
+$prod = $a * $b;
+$a += 5;
+
+echo "1) \nSum = $sum prod = $prod a = $a\n";
+echo "2)\n", 2 + 3 * 4, "\n";
+echo (2+3) * 4, "\n";
+echo "3) \n";
+
+$name = "Ada";
+echo "Hello $name \n";
+echo 'Hello ' . $name . "\n";
+
+$x = "0";
+var_dump($x == false);
+var_dump($x === false);
+
+?>
